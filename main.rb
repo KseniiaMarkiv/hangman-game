29cf7f882a7906c63c9b5dba1ee8838f72460b72
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-08-05 13:50:52 -0400

    Added colorize gem

--- main.rb
+++ main.rb
@@ -1,3 +1,26 @@
+require 'colorize'
+# * colorizing methods
+
+def blue_color(string)
+  string.colorize(:blue)
+end
+
+def yellow_color(string)
+  string.colorize(:yellow)
+end
+
+def green_color(string)
+  string.colorize(:green)
+end
+
+def red_color(string)
+  string.colorize(:red)
+end
+
+def gray_color(string)
+  string.colorize(:gray)
+end
+
 # all constants of symbols
 CELEBRATION_SYMBOL = "\u{1F389}"
 RAINBOW_SYMBOL = "\u{1F308}"
@@ -5,17 +28,16 @@ COOL_EMOJI = "\u{1F60E}"
 MUSHROOM_EMOJI = "\u{1F344}"
 AIM_SYMBOL = "\u{1F3AF}"
 
+# display some sort of count so the player knows how many more incorrect guesses they have before the game ends
+MAX_TURNS = 8
+
 
 # your script should load in the dictionary and randomly select a word between 5 and 12 characters
-
 path_file = 'D:\00_main_job\main\English\23_11_interview\Course\repos\hangman-game\google-10000-english-no-swears.txt'
 
 words = File.readlines(path_file).map(&:chomp).select { |word| word.length.between?(5, 12) }
 random_word = words.sample
 puts random_word
-
-# display some sort of count so the player knows how many more incorrect guesses they have before the game ends
-MAX_TURNS = 8
 
 def initialize_turns
   Array.new(8, ' ')
@@ -105,8 +127,6 @@ def display_hangman(incorrect_guesses)
   puts hangman_stages[incorrect_guesses]
 end
 
-display_hangman(6)
-
 def turns_taken
   1
 end
@@ -114,6 +134,4 @@ end
 def message_for_players
   puts "You have #{MAX_TURNS - turns_taken} turns left."
 end
-message_for_players
-
 
